c66fdfda0f6a73b656d1230f44c4c4766b6a10ba
chris3evans <chris3evans.ce@gmail.com>
2022-12-18 21:41:08 +0000

    Added env variables to sequelize connection

--- server/Models/index.js
+++ server/Models/index.js
@@ -1,21 +1,30 @@
 const environment = require("../../my-fitness-tracker/src/Utils/environment.js");
+// const url = process.env.NODE_ENV ? API_URL_PRODUCTION : API_URL_DEVELOPMENT;
+const DATABASE_NAME = process.env.NODE_ENV
+  ? process.env.DATABASE
+  : environment.DATABASE_NAME;
+const DATABASE_PASSWORD = process.env.NODE_ENV
+  ? process.env.PASSWORD
+  : environment.PASSWORD;
+const HOST = process.env.NODE_ENV ? process.env.HOST : environment.HOST;
+const PORT = process.env.NODE_ENV ? process.env.PORT : environment.PORT;
+// Database name
+// Database password
+// Host name (local etc)
+// Port number (5432)
 
 const Sequelize = require("sequelize");
 const fs = require("fs");
 const path = require("path");
+const { env } = require("process");
 const db = {};
 
-const sequelize = new Sequelize(
-  environment.DATABASE_NAME,
-  "postgres",
-  environment.PASSWORD,
-  {
-    host: environment.HOST,
-    port: environment.PORT,
-    dialect: "postgres",
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(DATABASE_NAME, "postgres", DATABASE_PASSWORD, {
+  host: HOST,
+  port: PORT,
+  dialect: "postgres",
+  logging: false,
+});
 
 const files = fs.readdirSync(__dirname);
 
