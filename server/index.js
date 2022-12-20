1bc0215913e408f9229a707b0e7f783d4ea49a02
chris3evans <chris3evans.ce@gmail.com>
2022-12-20 12:35:21 +0000

    Added back static file serving

--- server/index.js
+++ server/index.js
@@ -2,23 +2,21 @@ const express = require("express");
 const app = express();
 const router = require("./router");
 const cors = require("cors");
-const port = process.env.PORT || 3333;
 const db = require("./Models/index");
 const path = require("path");
 
 app.use(express.json());
 app.use(cors());
 
-// app.use(express.static(path.join(__dirname, "../my-fitness-tracker/build")));
+app.use(express.static(path.join(__dirname, "../my-fitness-tracker/build")));
 
 app.use(router);
 
-// app.get("*", (req, res) => {
-//   res.sendFile(
-//     path.join(__dirname + "/../my-fitness-tracker/build/index.html")
-//   );
-// })
-(async () => {
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname + "/../my-fitness-tracker/build/index.html")
+  );
+})(async () => {
   await db.sequelize.sync();
   console.log("🖥️🖥️🖥️ Postgres database is running with sequelize");
 
