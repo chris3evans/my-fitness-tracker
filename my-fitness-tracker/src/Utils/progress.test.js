21dfef781bb5752891cc3705b35914824c6426b3
chris3evans <chris3evans.ce@gmail.com>
2022-12-27 12:32:53 +0000

    Wrote tests for all working outcomes for checkResistanceProgress function

--- my-fitness-tracker/src/Utils/progress.test.js
+++ my-fitness-tracker/src/Utils/progress.test.js
@@ -21,12 +21,38 @@ const mockData4 = {
   sets: 1,
   reps: 1,
 };
+const mockData5 = {
+  maxweight: 2,
+  sets: 2,
+  reps: 2,
+};
+const mockData6 = {
+  maxweight: 1,
+  sets: 2,
+  reps: 2,
+};
+const mockData7 = {
+  maxweight: 2,
+  sets: 1,
+  reps: 2,
+};
+const mockData8 = {
+  maxweight: 2,
+  sets: 2,
+  reps: 1,
+};
 
 describe("checkResistanceProgress()", () => {
-  it('should return a row of "same" if first row of data', () => {
+  it('should return 3 "same" if first row of data', () => {
     const input = [mockData1];
     const result = checkResistanceProgress(input);
     expect(result).toStrictEqual(["same", "same", "same"]);
+  });
+
+  it('should return 3 "same" if weight, sets and reps are equal to the previous session', () => {
+    const input = [mockData1, mockData1];
+    const result = checkResistanceProgress(input);
+    expect(result[1]).toStrictEqual(["same", "same", "same"]);
   });
 
   it('should return 3 "improve" even if just weight is greater than previous session', () => {
@@ -35,7 +61,7 @@ describe("checkResistanceProgress()", () => {
     expect(result[1]).toStrictEqual(["improve", "improve", "improve"]);
   });
 
-  it('should return 2 "improve" if for sets being greater than previous session', () => {
+  it('should return 2 "improve" for sets being greater than previous session', () => {
     const input = [mockData1, mockData3];
     const result = checkResistanceProgress(input);
     expect(result[1]).toStrictEqual(["same", "improve", "improve"]);
@@ -45,5 +71,23 @@ describe("checkResistanceProgress()", () => {
     const input = [mockData1, mockData2];
     const result = checkResistanceProgress(input);
     expect(result[1]).toStrictEqual(["same", "same", "improve"]);
+  });
+
+  it('should return 3 "worse" even if just weight is less than previous session', () => {
+    const input = [mockData5, mockData6];
+    const result = checkResistanceProgress(input);
+    expect(result[1]).toStrictEqual(["worse", "worse", "worse"]);
+  });
+
+  it('should return 2 "worse" for sets being less than previous session', () => {
+    const input = [mockData5, mockData7];
+    const result = checkResistanceProgress(input);
+    expect(result[1]).toStrictEqual(["same", "worse", "worse"]);
+  });
+
+  it('should return 1 "worse" if just reps are worse than previous session', () => {
+    const input = [mockData5, mockData8];
+    const result = checkResistanceProgress(input);
+    expect(result[1]).toStrictEqual(["same", "same", "worse"]);
   });
 });
